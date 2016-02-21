7236534c6ecd3b2a70f1cfbec7cc3047183a5f7f
Andrew <browner12@gmail.com>
2016-02-21 01:11:27 -0600

    create a helper generator

---
+++ src/Commands/HelperMakeCommand.php
@@ -0,0 +1,48 @@
+<?php namespace browner12\helpers\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class HelperMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:helper';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new helper';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Helper';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/stubs/helper.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Helpers';
+    }
+}
---
+++ src/Commands/stubs/helper.stub
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('DummyFunction')) {
+
+    /**
+     * description
+     *
+     * @param
+     * @return
+     */
+    function DummyFunction()
+    {
+
+    }
+}
