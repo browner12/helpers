59817d19bbcf0cffe5f6e36908f387d52f35d5cd
Andrew <browner12@gmail.com>
2016-02-21 01:11:39 -0600

    create a default config file

---
+++ src/config/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Active Helpers
+    |--------------------------------------------------------------------------
+    |
+    | This package comes with many pre-built helpers. Activate the ones
+    | you wish to use by selecting them here. Valid options include:
+    |
+    | buttons
+    | confirm
+    | datetime
+    | error
+    | feedback
+    | formatter
+    | hint
+    | image
+    | money
+    | validation
+    |
+    */
+    'active_helpers' => [
+    ],
+
+];
